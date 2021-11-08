64d29bb7ed9cae11a51a31c494290b3ab4d3e285
moacirescorcio <moacirescorcio@gmail.com>
2021-11-08 17:24:18 -0300

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,15 +1,14 @@
 def quantos_anos(d, j):
     quantidade = 0
-    while d > 0 and j > 0:
-        d * j == 2*d
-        quantidade += 1
-
+    while True:
+        d * (j/100) + d <= 2 * d
+        quantidade =+ 1
     return quantidade
 
 
 
 
-deposito_inicial = float(input())
-juros = float(input())
-anos = quantos_anos(deposito_inicial, juros)
+deposito = int(input())
+juros = int(input())
+anos = quantos_anos(deposito, juros)
 print(anos)
